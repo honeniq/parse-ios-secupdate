dc8dadafa6c34a5ee32900922053e5bbc063cb8e
honeniq <honeniq@gmail.com>
2015-01-14 16:55:37 +0900

    rubocopでチェックした

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
-#gem 'open-uri'
+# gem 'open-uri'
 gem 'nokogiri'
 gem 'pry'
--- scrape.rb
+++ scrape.rb
@@ -5,9 +5,9 @@ require 'csv'
 require 'pry'
 
 if ARGV.count == 0
-  url = "http://support.apple.com/ja-jp/HT6590"
+  url = 'http://support.apple.com/ja-jp/HT6590'
 else
-  url = ARGV[0] 
+  url = ARGV[0]
 end
 
 charset = nil
@@ -19,12 +19,11 @@ end
 doc = Nokogiri::HTML.parse(html, nil, charset)
 m = url.match(%r{http://.*.apple.com/.*/(.+?)$})
 output = m[1] + '.csv'  # match結果は配列の[1]に入る
-csv = CSV.open(output,"a:utf-8")
-
+csv = CSV.open(output, 'a:utf-8')
 
 # Appleの脆弱性情報は1件ごとにtype=circleのulでまとめられている
 doc.xpath('//ul[@type="circle"]/li').each do |li|
-  buf = [] 
+  buf = []
   li.xpath('p').each do | lip |
     buf.push(lip.text)
   end
@@ -33,5 +32,5 @@ end
 
 # BOM挿入(\uFEFF)
 open(output, 'r+:utf-8') do |f|
-  f.puts "\uFEFF#{open(output, "r:utf-8").read}"
+  f.puts "\uFEFF#{open(output, 'r:utf-8').read}"
 end
